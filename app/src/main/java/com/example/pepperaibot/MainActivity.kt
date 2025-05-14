60e6bfef9adfa7e906836b615ebbcfe36875aa3e
Juho Maijala <juho.maijala@tuni.fi>
2025-05-14 12:31:55 +0300

    Add necessary pepper dependencies

--- app/src/main/java/com/example/pepperaibot/MainActivity.kt
+++ app/src/main/java/com/example/pepperaibot/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.pepperaibot
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pepperaibot.ui.theme.PepperAIBotTheme
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), RobotLifecycleCallbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        QiSDK.register(this, this)
         enableEdgeToEdge()
         setContent {
             PepperAIBotTheme {
@@ -28,6 +34,20 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    override fun onDestroy() {
+        QiSDK.unregister(this, this)
+        super.onDestroy()
+    }
+
+    override fun onRobotFocusGained(qiContext: QiContext?) {
+    }
+
+    override fun onRobotFocusLost() {
+    }
+
+    override fun onRobotFocusRefused(reason: String?) {
+    }
+
 }
 
 @Composable
