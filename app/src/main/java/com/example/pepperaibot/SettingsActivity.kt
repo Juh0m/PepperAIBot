56e9a669cc6c441d150f38379b5002c4a657b63e
Juh0m <141812817+Juh0m@users.noreply.github.com>
2025-06-04 11:41:34 +0300

    Scroll in settings menu (#23)
    * Ok lol pls now worke
    
    * Add timeout setting back
    
    ---------
    
    Co-authored-by: Topsanopsa <topi.nikula@elisanet.fi>

--- app/src/main/java/com/example/pepperaibot/SettingsActivity.kt
+++ app/src/main/java/com/example/pepperaibot/SettingsActivity.kt
@@ -1,14 +1,30 @@
 package com.example.pepperaibot
 
-import android.util.Log
 import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
@@ -57,9 +73,12 @@ fun SettingsScreen() {
         mutableStateOf(sharedPreferences.getLong("api_read_timeout", 30).toString())
     }
 
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(scrollState)
             .padding(16.dp),
         verticalArrangement = Arrangement.Top
     ) {
@@ -165,6 +184,7 @@ fun SettingsScreen() {
                 label = { Text("Voice Recognition Model") },
                 modifier = Modifier.fillMaxWidth()
             )
+
         }
         // Wait timeout
         Spacer(modifier = Modifier.height(16.dp))
@@ -185,3 +205,4 @@ fun SettingsScreen() {
         )
     }
 }
+
